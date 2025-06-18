427b4f5ab32f2ea577a7cbd84acb91d41e193c8b
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2025-06-18 19:07:37 -0300

    add get message by id function

--- src/main/kotlin/com/example/first_crud/MessageController.kt
+++ src/main/kotlin/com/example/first_crud/MessageController.kt
@@ -1,14 +1,30 @@
 package com.example.first_crud
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
-class MessageController{
+@RequestMapping("/")
+class MessageController(private val service: MessageService) {
     @GetMapping("/")
-    fun listMessages() = listOf(
-        Message("1", "Hola!"),
-        Message("2", "Hello!"),
-        Message("3", "Bonjour!")
-    )
+    fun listMessages() = ResponseEntity.ok(service.findMessages())
+
+    @PostMapping
+    fun post(@RequestBody message: Message): ResponseEntity<Message> {
+        val savedMessage = service.save(message)
+        return ResponseEntity.created(URI("/${savedMessage.id}")).body(savedMessage)
+    }
+
+    @GetMapping("/{id}")
+    fun getMessage(@PathVariable id: String): ResponseEntity<Message> =
+        service.findMessageById(id).toResponseEntity()
+
+    private fun Message?.toResponseEntity(): ResponseEntity<Message> =
+        this?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
 }
