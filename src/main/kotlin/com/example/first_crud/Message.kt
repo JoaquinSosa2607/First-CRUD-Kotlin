a7bd94b5ddb94eea350d935485d12e10b99fb8fc
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2025-06-18 18:28:59 -0300

    add message data class and change message controller function

---
+++ src/main/kotlin/com/example/first_crud/Message.kt
@@ -0,0 +1,3 @@
+package com.example.first_crud
+
+data class Message(val id: String?, val text: String)
--- src/main/kotlin/com/example/first_crud/MessageController.kt
+++ src/main/kotlin/com/example/first_crud/MessageController.kt
@@ -1,11 +1,14 @@
 package com.example.first_crud
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MessageController{
     @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hi, $name!"
+    fun listMessages() = listOf(
+        Message("1", "Hola!"),
+        Message("2", "Hello!"),
+        Message("3", "Bonjour!")
+    )
 }
