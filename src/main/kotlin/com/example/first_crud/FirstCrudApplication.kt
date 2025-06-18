3f884f2d651d6e934d684eb476c8ebb888df6f9d
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2025-06-18 18:19:44 -0300

    first commit

---
+++ src/main/kotlin/com/example/first_crud/FirstCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.first_crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstCrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstCrudApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/first_crud/FirstCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.first_crud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstCrudApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
